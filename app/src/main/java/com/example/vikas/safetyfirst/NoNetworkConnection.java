3d54b56ae35924f7416b19cf5b4f6b47f4348f9b
Vivek Singh Bhadauria <viveksbhadauria007@gmail.com>
2016-09-10 12:43:18 +0530

    links blocked,Network Connection Checked and handled,progress bar in news loading : all done

---
+++ app/src/main/java/com/example/vikas/safetyfirst/NoNetworkConnection.java
@@ -0,0 +1,18 @@
+package com.example.vikas.safetyfirst;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+/**
+ * Created by viveksb007 on 10/9/16.
+ */
+public class NoNetworkConnection extends BaseActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_no_network);
+        ImageView imgNoNetwork = (ImageView)findViewById(R.id.img_no_network);
+        imgNoNetwork.setImageResource(R.drawable.no_internet_connection);
+    }
+}
--- app/src/main/java/com/example/vikas/safetyfirst/mDiscussion/fragment/SearchPostFragment.java
+++ app/src/main/java/com/example/vikas/safetyfirst/mDiscussion/fragment/SearchPostFragment.java
@@ -6,12 +6,9 @@ import android.widget.Toast;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.Query;
 
-import java.util.List;
-
 public class SearchPostFragment extends PostListFragment {
 
     private String query;
-    String[] keywords;
 
     public static SearchPostFragment newInstance(String query) {
         SearchPostFragment postFragment = new SearchPostFragment();
@@ -23,12 +20,12 @@ public class SearchPostFragment extends PostListFragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Toast.makeText(getContext(), query, Toast.LENGTH_SHORT).show();
-        keywords = query.split(" ");
     }
 
     @Override
     public Query getQuery(DatabaseReference databaseReference) {
-        if (databaseReference.child("keywords").child(query.toLowerCase()) == null) {
+        // TODO : Check if child exists , this method is not working
+        if (databaseReference.child("keywords").child(query.toLowerCase()).getRef() == null) {
             Toast.makeText(getContext(), "No TAGS Found. Try another word.", Toast.LENGTH_SHORT).show();
         }
         return databaseReference.child("keywords").child(query.toLowerCase());
