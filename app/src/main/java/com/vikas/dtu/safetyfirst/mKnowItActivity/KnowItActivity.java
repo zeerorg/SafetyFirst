4d079cd37a5dfa18a9974d524a239abfa3167db2
vikasmahato <vikasmahato0@gmail.com>
2016-10-23 01:31:30 +0530

    Added Know It

--- app/src/main/java/com/vikas/dtu/safetyfirst/mKnowItActivity/KnowItActivity.java
+++ app/src/main/java/com/vikas/dtu/safetyfirst/mKnowItActivity/KnowItActivity.java
@@ -4,23 +4,19 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.ActionBar;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.vikas.dtu.safetyfirst.BaseActivity;
 import com.vikas.dtu.safetyfirst.R;
-import com.vikas.dtu.safetyfirst.SignInActivity2;
-import com.vikas.dtu.safetyfirst.mKnowItActivity.Ladder.LadderActivity;
-import com.vikas.dtu.safetyfirst.mKnowItActivity.Ladder.SelfSupportingLadder;
-import com.vikas.dtu.safetyfirst.mKnowItActivity.Scaffoldings.Scaffolding;
+import com.vikas.dtu.safetyfirst.mSignUp.SignInGoogle;
 
 /**
  * Created by krishna on 16/10/16.
  */
-
 public class KnowItActivity extends BaseActivity {
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,7 +30,7 @@ public class KnowItActivity extends BaseActivity {
     }
 
     public void ladderActivity(View view) {
-        Intent intent = new Intent(this, LadderActivity.class);
+        Intent intent = new Intent(this, com.vikas.dtu.safetyfirst.mKnowItActivity.Ladder.LadderActivity.class);
         startActivity(intent);
     }
 
@@ -44,7 +40,7 @@ public class KnowItActivity extends BaseActivity {
         int i = item.getItemId();
         if (i == R.id.action_logout) {
             FirebaseAuth.getInstance().signOut();
-            startActivity(new Intent(this, SignInActivity2.class));
+            startActivity(new Intent(this, SignInGoogle.class));
             finish();
             return true;
         } else {
@@ -53,7 +49,7 @@ public class KnowItActivity extends BaseActivity {
     }
 
     public void scaffolding(View view) {
-        Intent intent = new Intent(this, Scaffolding.class);
+        Intent intent = new Intent(this, com.vikas.dtu.safetyfirst.mKnowItActivity.Scaffoldings.Scaffolding.class);
         startActivity(intent);
 
     }
