6874dc8b1f061feb64d54a25ec860a01cf0e41d4
vikasmahato <vikasmahato0@gmail.com>
2016-11-02 17:26:13 +0530

    Minor UI changes in news. Added rudimentary Laws section

---
+++ app/src/main/java/com/vikas/dtu/safetyfirst/mLaws/LawsActivity.java
@@ -0,0 +1,15 @@
+package com.vikas.dtu.safetyfirst.mLaws;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.vikas.dtu.safetyfirst.R;
+
+public class LawsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_laws);
+    }
+}
